5ea6f841fcce57841ffed96a276a6a2141a7a6eb
Mekhi-M <mekhim0731@gmail.com>
2024-06-11 14:30:17 -0400

    fin

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,16 +1,49 @@
-const resolvedWrapper = () => {
+const resolvedWrapper = (val) => {
+  return new Promise((resolve, reject) => resolve(val));
 };
 
-const rejectedWrapper = () => {
+// // Q1 example input/output
+// console.log(resolvedWrapper(10));
+// // this returns:
+// // Promise { 10 }
+
+// // and we could log it out like this:
+// resolvedWrapper(10).then(console.log);
+// // this logs:
+// // 10
+
+const rejectedWrapper = (str) => {
+  return new Promise((resolve, reject) => reject(new Error(str)));
 };
 
-const handleResolvedPromise = () => {
+// // Q2 example input/output
+// console.log(rejectedWrapper("bruh"));
+// // this returns:
+// // Promise { Error: bruh }
+
+const handleResolvedPromise = (promise) => {
+  return promise.then((result) => {
+    console.log(result);
+    return result;
+  });
 };
 
-const handleResolvedOrRejectedPromise = () => {
+const handleResolvedOrRejectedPromise = (promise) => {
+  return promise
+    .then((result) => {
+      console.log(result);
+      return result;
+    })
+    .catch((error) => {
+      console.error(`Your error message was: ${error.message}`);
+      return null;
+    });
 };
 
-const pauseForMs = () => {
+const pauseForMs = (milliseconds) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
 };
 
 module.exports = {
